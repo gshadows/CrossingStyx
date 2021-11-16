90cb58428e783547c232b59899d8233215c595e3
G-Shadow <18046412+gshadows@users.noreply.github.com>
2021-11-17 00:02:15 +0300

    Move editor helper class to separate folder. Remove audio asset duplicate.

--- Assets/Scripts/editor_tools/ReadOnlyProperty.cs
+++ Assets/Scripts/editor_tools/ReadOnlyProperty.cs
@@ -6,14 +6,11 @@ public class ReadOnlyAttribute : PropertyAttribute {}
 #if UNITY_EDITOR
 [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
 public class ReadOnlyProperty : PropertyDrawer {
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
-        return EditorGUI.GetPropertyHeight(property, label, true);
-    }
-
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        bool savedGuiEnable = GUI.enabled;
         GUI.enabled = false;
         EditorGUI.PropertyField(position, property, label, true);
-        GUI.enabled = true;
+        GUI.enabled = savedGuiEnable;
     }
 }
 #endif
